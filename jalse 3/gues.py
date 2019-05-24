d73a2d5d8e823568d8460eb5df3be42039535971
hamid <ha226699@gmail.com>
2019-05-24 09:05:09 +0330

    soale hads zadan

---
+++ jalse 3/gues.py
@@ -0,0 +1,13 @@
+secrt = 8
+
+while True:
+    gus = int(input('adadeto bezan :'))
+    if gus < secrt:
+        print("doros nis")
+    elif gus > secrt :
+        print("dorost nis baz dadash")
+    elif gus==secrt:
+        print("dorosee")
+        break
+
+
