a75e0587e03f2d50af050a07f79e9af4ff2123f3
hamid <ha226699@gmail.com>
2019-06-14 00:05:18 +0430

    set and upper

---
+++ jalase 2/operator.py
@@ -0,0 +1,4 @@
+list_1 = [1, 2, 3, 4]
+list_2 = [5, 6, 7, 8]
+lst = list_1 + list_2
+print(lst)
---
+++ jalase 2/qusetion1.py
@@ -0,0 +1,8 @@
+tet = "hi word what are you doing "
+kar = tet.upper()
+print(kar)
+my_lst = set(kar)
+print(my_lst)
+my_lsst = [1, 2, 2, 3, 4, 5,5 ,6]
+inoo = set(my_lsst)
+print(inoo)
