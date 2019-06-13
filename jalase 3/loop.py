fc07eabc40d39a28b64356ae89f186ee8804aa03
hamid <ha226699@gmail.com>
2019-06-14 01:11:10 +0430

    prime

---
+++ jalase 3/loop.py
@@ -0,0 +1,16 @@
+number = 111
+
+i = 2
+Is_perim = True
+
+while i < number:
+         if number % i == 0:
+           Is_perim = False
+           break
+         i = 1 + i
+
+
+
+print(Is_perim)
+
+
