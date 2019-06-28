1f44ed109bb394b9be9b06d21f3af42568a2d0e9
hamid <ha226699@gmail.com>
2019-06-28 12:57:15 +0330

    tamrine akhar

---
+++ jalase 6/test.py
@@ -0,0 +1,11 @@
+#def file(w)
+poem = open('shere').read()
+# out_file = open.shre
+lower_case = poem.lower()
+# out=out_file.write()
+
+print(len(poem.split()))
+print(len(set(poem.split())))
+
+##word= text.split(t1)
+
